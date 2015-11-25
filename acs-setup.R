dca9598702036ee857be964c9e28186e07832aa8
Hannes Mühleisen <hannes@muehleisen.org>
2015-11-25 23:27:36 +0100

    7zip

--- acs-setup.R
+++ acs-setup.R
@@ -1,4 +1,5 @@
 setwd(Sys.getenv("RWD"))
+path.to.7z <- "7za"
 single.year.datasets.to.download <- 2005:2014
 three.year.datasets.to.download <- 2007:2013
 five.year.datasets.to.download <- 2009:2013
