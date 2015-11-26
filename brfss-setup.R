f35b5198f8823e85c3051f9d88c5949c58fe90a6
Hannes Muehleisen <hannes@muehleisen.org>
2015-11-26 14:28:56 +0100

    brfss windows

--- brfss-setup.R
+++ brfss-setup.R
@@ -1,4 +1,10 @@
 setwd(Sys.getenv("RWD"))
-options(encoding="windows-1252")
+if (.Platform$OS.type == 'windows') {
+	setInternet2(FALSE)	
+	options(monetdb.programpath.override=Sys.getenv("MONETDBINSTALLDIR"))
+}
+else {
+	options(encoding="windows-1252")
+}
 years.to.download <- 1984:2014
 downloader::source_url( "https://raw.github.com/ajdamico/asdfree/master/Behavioral%20Risk%20Factor%20Surveillance%20System/download%20all%20microdata.R" , prompt = FALSE , echo = TRUE)
--- brfss-test.R
+++ brfss-test.R
@@ -1,6 +1,11 @@
 setwd(Sys.getenv("RWD"))
-batfile <- paste0(getwd(),"/MonetDB/brfss.sh")
-load( paste0(getwd(),'/b2011 design.rda' ))
+if (.Platform$OS.type == 'windows') {
+	batfile <- paste0(getwd(),"/MonetDB/brfss.bat")
+}
+else {
+	batfile <- paste0(getwd(),"/MonetDB/brfss.sh")
+}
+load(paste0(getwd(), '/b2011 design.rda'))
 downloader::source_url("https://raw.github.com/ajdamico/asdfree/master/Behavioral%20Risk%20Factor%20Surveillance%20System/2011%20single-year%20-%20analysis%20examples.R" , prompt = FALSE , echo = TRUE )
 
 
