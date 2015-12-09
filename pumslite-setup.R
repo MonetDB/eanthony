171f30a97ce93b2c7fffd94ebc78c425af1ecc90
Hannes Muehleisen <hannes@muehleisen.org>
2015-12-09 11:51:00 +0100

    pums

--- pumslite-setup.R
+++ pumslite-setup.R
@@ -4,5 +4,5 @@ one.percent.files.to.download <- c( 1990 , 2000 )
 five.percent.files.to.download <- c( 1990 , 2000 )
 ten.percent.files.to.download <- 2010
 exclude.puerto.rico <- TRUE
-downloader::source_url( "https://raw.github.com/ajdamico/asdfree/master/United%20States%20Decennial%20Census%20Public%20Use%20Microdata%20Sample/download%20and%20import.R" , prompt = FALSE , echo = TRUE )
+downloader::source_url( "https://raw.githubusercontent.com/hannesmuehleisen/asdfree/master/United%20States%20Decennial%20Census%20Public%20Use%20Microdata%20Sample/download%20and%20import.R" , prompt = FALSE , echo = TRUE )
 
