272dafcf0465e26bd484edd434b6eaabf3545e4e
Hannes Muehleisen <hannes@bricks01.scilens.private>
2015-12-14 08:32:45 +0100

    nirbs changes

--- nibrs-setup.R
+++ nibrs-setup.R
@@ -1,5 +1,5 @@
 setwd(Sys.getenv("RWD"))
 options(monetdb.debug.query=T)
-your.username <- 'some'
-your.password <- 'thing'
+your.username <- 'hannes@cwi.nl'
+your.password <- 'cwicwi'
 downloader::source_url("https://raw.github.com/ajdamico/asdfree/master/National%20Incident-Based%20Reporting%20System/download%20all%20microdata.R" , prompt = FALSE, echo = TRUE)
