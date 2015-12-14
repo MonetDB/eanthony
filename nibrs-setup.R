7ef8281f4642abb9f2d51b7f72b2a827891dd8d5
Hannes Mühleisen <hannes@muehleisen.org>
2015-12-14 08:31:58 +0100

    no query logging for nirbs

--- nibrs-setup.R
+++ nibrs-setup.R
@@ -1,5 +1,4 @@
 setwd(Sys.getenv("RWD"))
-options(monetdb.debug.query=T)
 your.username <- 'some'
 your.password <- 'thing'
 downloader::source_url("https://raw.github.com/ajdamico/asdfree/master/National%20Incident-Based%20Reporting%20System/download%20all%20microdata.R" , prompt = FALSE, echo = TRUE)
--- nibrslite-setup.R
+++ nibrslite-setup.R
@@ -1,5 +1,4 @@
 setwd(Sys.getenv("RWD"))
-options(monetdb.debug.query=T)
 your.username <- 'hannes@cwi.nl'
 your.password <- 'fuuu'
 downloader::source_url("https://raw.githubusercontent.com/hannesmuehleisen/asdfree/master/National%20Incident-Based%20Reporting%20System/download%20all%20microdata.R" , prompt = FALSE, echo = TRUE)
