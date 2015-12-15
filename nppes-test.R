5af196e3647a262330e3ec16a98a50788689e710
Anthony Damico <ajdamico@gmail.com>
2015-12-15 07:35:54 -0500

    asdfree nppes now uses monetdblite so abandon other tests

--- nppes-test.R
+++ nppes-test.R
@@ -1,4 +1,6 @@
 setwd(Sys.getenv("RWD"))
-batfile <- paste0(getwd(),"/nppes.sh")
+options(monetdb.debug.query=T)
+
 options( encoding = "windows-1252" )
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/National%20Plan%20and%20Provider%20Enumeration%20System/replicate%20cms%20state%20counts.R" , prompt = FALSE , echo = TRUE )
+downloader::source_url( "https://raw.github.com/ajdamico/asdfree/master/National%20Plan%20and%20Provider%20Enumeration%20System/merge%20taxonomy%20ids.R" , prompt = FALSE , echo = TRUE )
--- nppeslite-test.R
+++
@@ -1,3 +0,0 @@
-setwd(Sys.getenv("RWD"))
-options( encoding = "windows-1252" )
-downloader::source_url( "https://raw.githubusercontent.com/hannesmuehleisen/asdfree/master/National%20Plan%20and%20Provider%20Enumeration%20System/replicate%20cms%20state%20counts.R" , prompt = FALSE , echo = TRUE )
