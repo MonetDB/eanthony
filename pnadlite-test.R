065a035e463e2e16e302f877e3cc669b4c1c5eac
Hannes Muehleisen <hannes@muehleisen.org>
2015-12-15 18:56:16 +0100

    fix urls

--- pnadlite-test.R
+++ pnadlite-test.R
@@ -1,15 +1,9 @@
 setwd(Sys.getenv("RWD"))
 options(monetdb.debug.query=T)
-
-
 options( encoding = "windows-1252" )		# # only macintosh and *nix users need this line
 year.to.analyze <- 2012
-downloader::source_url( "https://raw.github.com/ajdamico/asdfree/master/Pesquisa%20Nacional%20por%20Amostra%20de%20Domicilios/single-year%20-%20analysis%20examples.R" , prompt = FALSE , echo = TRUE )
+downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Pesquisa%20Nacional%20por%20Amostra%20de%20Domicilios/single-year%20-%20analysis%20examples.R" , prompt = FALSE , echo = TRUE )
 
+downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Pesquisa%20Nacional%20por%20Amostra%20de%20Domicilios/replicate%20IBGE%20estimates%20-%202011.R" , prompt = FALSE , echo = TRUE )
 
-options( encoding = "windows-1252" )		# # only macintosh and *nix users need this line
-downloader::source_url( "https://raw.github.com/ajdamico/asdfree/master/Pesquisa%20Nacional%20por%20Amostra%20de%20Domicilios/replicate%20IBGE%20estimates%20-%202011.R" , prompt = FALSE , echo = TRUE )
-
-
-options( encoding = "windows-1252" )		# # only macintosh and *nix users need this line
-downloader::source_url( "https://raw.github.com/ajdamico/asdfree/master/Pesquisa%20Nacional%20por%20Amostra%20de%20Domicilios/2011%20single-year%20-%20variable%20recode%20example.R" , prompt = FALSE , echo = TRUE )
+downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Pesquisa%20Nacional%20por%20Amostra%20de%20Domicilios/2011%20single-year%20-%20variable%20recode%20example.R" , prompt = FALSE , echo = TRUE )
