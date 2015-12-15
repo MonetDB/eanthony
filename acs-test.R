b39cebc6dafdd34585863a543d05e36787521976
Anthony Damico <ajdamico@gmail.com>
2015-12-15 17:46:10 -0500

    main acs now uses monetdblite so no more need for acslite

--- acs-test.R
+++ acs-test.R
@@ -1,11 +1,4 @@
 setwd(Sys.getenv("RWD"))
 options(monetdb.debug.query=T)
-if (.Platform$OS.type == 'windows') {
-	batfile <- paste0(getwd(),"/MonetDB/acs.bat")
-}
-else {
-	batfile <- paste0(getwd(),"/MonetDB/acs.sh")
-}
-load('acs2011_1yr.rda')
 downloader::source_url( "https://raw.github.com/ajdamico/asdfree/master/American%20Community%20Survey/replicate%20census%20estimates%20-%202011.R" , prompt = FALSE , echo = TRUE )
 downloader::source_url( "https://raw.github.com/ajdamico/asdfree/master/American%20Community%20Survey/2011%20single-year%20-%20analysis%20examples.R" , prompt = FALSE , echo = TRUE )
