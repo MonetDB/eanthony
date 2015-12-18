dcaacc0dc842ee30fdd491fc54616b7cda97aac1
Anthony Damico <ajdamico@gmail.com>
2015-12-18 10:59:33 -0500

    pnad retry

--- cps-setup.R
+++ cps-setup.R
@@ -1,8 +1,3 @@
 setwd(Sys.getenv("RWD"))
-
-if (.Platform$OS.type == 'windows') {
-	options(monetdb.programpath.override=Sys.getenv("MONETDBINSTALLDIR"))
-} 
-
 cps.years.to.download <- c( 2015 , 2014 , 2014.58 , 2014.38 , 2013:1998 )
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Current%20Population%20Survey/download%20all%20microdata.R" , prompt = FALSE , echo = TRUE )
--- pnadlite-test.R
+++ pnadlite-test.R
@@ -5,5 +5,3 @@ year.to.analyze <- 2012
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Pesquisa%20Nacional%20por%20Amostra%20de%20Domicilios/single-year%20-%20analysis%20examples.R" , prompt = FALSE , echo = TRUE )
 
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Pesquisa%20Nacional%20por%20Amostra%20de%20Domicilios/replicate%20IBGE%20estimates%20-%202011.R" , prompt = FALSE , echo = TRUE )
-
-downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Pesquisa%20Nacional%20por%20Amostra%20de%20Domicilios/2011%20single-year%20-%20variable%20recode%20example.R" , prompt = FALSE , echo = TRUE )
