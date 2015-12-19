ad7bcd883c1bb7e6419c99e5104a001d1616dab1
Anthony Damico <ajdamico@gmail.com>
2015-12-19 16:38:29 -0500

    pisa integration

--- pisa-setup.R
+++ pisa-setup.R
@@ -1,5 +1,5 @@
 setwd(Sys.getenv("RWD"))
 options(encoding="windows-1252")
 years.to.download <- c( 2000 , 2003 , 2006 , 2009 , 2012 )
-downloader::source_url( "https://raw.github.com/ajdamico/asdfree/master/Program%20for%20International%20Student%20Assessment/download%20import%20and%20design.R" , prompt = FALSE , echo = TRUE )
+downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Program%20for%20International%20Student%20Assessment/download%20import%20and%20design.R" , prompt = FALSE , echo = TRUE )
 
--- pisalite-test.R
+++
@@ -1,3 +0,0 @@
-setwd(Sys.getenv("RWD"))
-load('2012 int_stu12_dec03.rda')
-downloader::source_url( "https://raw.githubusercontent.com/hannesmuehleisen/asdfree/master/Program%20for%20International%20Student%20Assessment/analysis%20examples.R" , prompt = FALSE , echo = TRUE )
