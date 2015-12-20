92f544058b594534ff7f2e1e5d899d7f9b888c45
Anthony Damico <ajdamico@gmail.com>
2015-12-20 17:32:38 -0500

    censo integration

--- censo-test.R
+++ censo-test.R
@@ -1,7 +1,3 @@
 setwd(Sys.getenv("RWD"))
-if (.Platform$OS.type == 'windows') {
-	options(monetdb.programpath.override=Sys.getenv("MONETDBINSTALLDIR"))
-}
-batfile <- paste0(getwd(),"/MonetDB/censo_demografico.sh")
-load('pes 2010 design.rda')
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Censo%20Demografico/analysis%20examples.R" , prompt = FALSE , echo = TRUE )
+downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Censo%20Demografico/variable%20recode%20example.R" , prompt = FALSE , echo = TRUE )
