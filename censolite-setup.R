0124bd8a46eee9a630797e0e5f96ebb1e1faf3d4
Anthony Damico <ajdamico@gmail.com>
2015-12-22 11:18:34 -0500

    try this branch

---
+++ censolite-setup.R
@@ -0,0 +1,7 @@
+setwd(Sys.getenv("RWD"))
+if (.Platform$OS.type == 'windows') {
+	setInternet2(FALSE)
+} else {
+	options(encoding="latin1")	
+}
+downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/censolite/Censo%20Demografico/download%20and%20import.R" , prompt = FALSE , echo = TRUE )
