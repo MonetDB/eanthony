b4d597adc8106bb28c25712b4cd53ff692f33c8d
Hannes Mühleisen <hannes@muehleisen.org>
2016-04-26 12:36:30 +0200

    Update sipp-setup.R
    added shutdown

--- sipp-setup.R
+++ sipp-setup.R
@@ -9,7 +9,7 @@ downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/mast
 db <- dbConnect( MonetDBLite() , paste0( getwd() , "/SIPP96" ) )
 corruption_trigger <- "select tables.name, columns.name, location from tables inner join columns on tables.id=columns.table_id left join storage on tables.name=storage.table and columns.name=storage.column where location is null"
 dbGetQuery( db , corruption_trigger )
-dbDisconnect( db )
+dbDisconnect( db, shutdown = TRUE)
 
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Survey%20of%20Income%20and%20Program%20Participation/2001%20panel%20-%20download%20and%20create%20database.R" , prompt = FALSE , echo = TRUE )
 
@@ -17,7 +17,7 @@ downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/mast
 db <- dbConnect( MonetDBLite() , paste0( getwd() , "/SIPP01" ) )
 corruption_trigger <- "select tables.name, columns.name, location from tables inner join columns on tables.id=columns.table_id left join storage on tables.name=storage.table and columns.name=storage.column where location is null"
 dbGetQuery( db , corruption_trigger )
-dbDisconnect( db )
+dbDisconnect( db, shutdown = TRUE)
 
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Survey%20of%20Income%20and%20Program%20Participation/2004%20panel%20-%20download%20and%20create%20database.R" , prompt = FALSE , echo = TRUE )
 
@@ -25,7 +25,7 @@ downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/mast
 db <- dbConnect( MonetDBLite() , paste0( getwd() , "/SIPP04" ) )
 corruption_trigger <- "select tables.name, columns.name, location from tables inner join columns on tables.id=columns.table_id left join storage on tables.name=storage.table and columns.name=storage.column where location is null"
 dbGetQuery( db , corruption_trigger )
-dbDisconnect( db )
+dbDisconnect( db, shutdown = TRUE)
 
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Survey%20of%20Income%20and%20Program%20Participation/2008%20panel%20-%20download%20and%20create%20database.R" , prompt = FALSE , echo = TRUE )
 
@@ -33,4 +33,4 @@ downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/mast
 db <- dbConnect( MonetDBLite() , paste0( getwd() , "/SIPP08" ) )
 corruption_trigger <- "select tables.name, columns.name, location from tables inner join columns on tables.id=columns.table_id left join storage on tables.name=storage.table and columns.name=storage.column where location is null"
 dbGetQuery( db , corruption_trigger )
-dbDisconnect( db )
+dbDisconnect( db, shutdown = TRUE)
