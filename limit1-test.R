3a15a51111078b4dcd508531a7c3cacb2aa307f4
Anthony Damico <ajdamico@gmail.com>
2016-04-28 02:15:11 -0400

    limit1-test would break even if setup worked

--- limit1-test.R
+++ limit1-test.R
@@ -1,7 +1,8 @@
 setwd(Sys.getenv("RWD"))
 
+pnad.dbfolder <- paste0( getwd() , "/MonetDB" )
 
 # corruption sniffing
-db <- dbConnect( MonetDBLite() , dbfolder )
+db <- dbConnect( MonetDBLite() , pnad.dbfolder )
 corruption_trigger <- "select tables.name, columns.name, location from tables inner join columns on tables.id=columns.table_id left join storage on tables.name=storage.table and columns.name=storage.column where location is null"
 dbGetQuery( db , corruption_trigger )
