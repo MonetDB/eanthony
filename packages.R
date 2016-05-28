ed3d7cfd82855dfa32e6c0ac4370f47e23f5f740
Anthony Damico <ajdamico@gmail.com>
2016-05-28 05:47:58 -0400

    speed-test addition

--- packages.R
+++ packages.R
@@ -1,4 +1,4 @@
-lp <- c("xlsx", "Hmisc", "R.utils", "RCurl", "RSQLite", "SAScii", "XML", "descr", "devtools", "digest", "downloader", "foreign", "gdata", "ggplot2", "haven", "httr", "lavaan.survey", "lme4", "mapproj", "maps", "memisc", "mitools", "plyr", "readr", "readxl", "reshape2", "sas7bdat", "sdcMicro", "sqldf", "stringr", "survey", "tools", "ff","ggplot2","scales","fields","mapproj","RColorBrewer","maptools","raster","rgeos","fields","mgcv","spatstat","rgdal")
+lp <- c("xlsx", "Hmisc", "R.utils", "RCurl", "RSQLite", "SAScii", "XML", "descr", "devtools", "digest", "downloader", "foreign", "gdata", "ggplot2", "haven", "httr", "lavaan.survey", "lme4", "mapproj", "maps", "memisc", "mitools", "plyr", "readr", "readxl", "reshape2", "sas7bdat", "sdcMicro", "sqldf", "stringr", "survey", "tools", "ff","ggplot2","scales","fields","mapproj","RColorBrewer","maptools","raster","rgeos","fields","mgcv","spatstat","rgdal","data.table")
 repos <- c("http://cran.rstudio.com/")
 # only install if required
 np <- lp[!(lp %in% installed.packages()[,"Package"])]
---
+++ speed-setup.R
@@ -0,0 +1,1 @@
+setwd(Sys.getenv("RWD"))
