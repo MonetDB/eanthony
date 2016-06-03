35b2c71322e7a94ee350ef80e276714c64d2a7ed
Anthony Damico <ajdamico@gmail.com>
2016-06-03 02:08:48 -0400

    shutdown after corruption sniffing

--- sipp-setup.R
+++ sipp-setup.R
@@ -9,7 +9,7 @@ warnings()
 db <- dbConnect( MonetDBLite() , dbfolder )
 cs <- dbGetQuery( db , "select tables.name, columns.name, location from tables inner join columns on tables.id=columns.table_id left join storage on tables.name=storage.table and columns.name=storage.column where location is null and tables.name not in ('tables', 'columns', 'users', 'querylog_catalog', 'querylog_calls', 'querylog_history', 'tracelog', 'sessions', 'optimizers', 'environment', 'queue', 'rejects', 'storage', 'storagemodel', 'tablestoragemodel')" )
 print(cs)
-stopifnot(nrow(cs) == 0)
+stopifnot(nrow(cs) == 0) ; dbDisconnect( db , shutdown = TRUE )
 
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Survey%20of%20Income%20and%20Program%20Participation/2001%20panel%20-%20download%20and%20create%20database.R" , prompt = FALSE , echo = TRUE )
 
@@ -19,7 +19,7 @@ warnings()
 db <- dbConnect( MonetDBLite() , dbfolder )
 cs <- dbGetQuery( db , "select tables.name, columns.name, location from tables inner join columns on tables.id=columns.table_id left join storage on tables.name=storage.table and columns.name=storage.column where location is null and tables.name not in ('tables', 'columns', 'users', 'querylog_catalog', 'querylog_calls', 'querylog_history', 'tracelog', 'sessions', 'optimizers', 'environment', 'queue', 'rejects', 'storage', 'storagemodel', 'tablestoragemodel')" )
 print(cs)
-stopifnot(nrow(cs) == 0)
+stopifnot(nrow(cs) == 0) ; dbDisconnect( db , shutdown = TRUE )
 
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Survey%20of%20Income%20and%20Program%20Participation/2004%20panel%20-%20download%20and%20create%20database.R" , prompt = FALSE , echo = TRUE )
 
@@ -29,7 +29,7 @@ warnings()
 db <- dbConnect( MonetDBLite() , dbfolder )
 cs <- dbGetQuery( db , "select tables.name, columns.name, location from tables inner join columns on tables.id=columns.table_id left join storage on tables.name=storage.table and columns.name=storage.column where location is null and tables.name not in ('tables', 'columns', 'users', 'querylog_catalog', 'querylog_calls', 'querylog_history', 'tracelog', 'sessions', 'optimizers', 'environment', 'queue', 'rejects', 'storage', 'storagemodel', 'tablestoragemodel')" )
 print(cs)
-stopifnot(nrow(cs) == 0)
+stopifnot(nrow(cs) == 0) ; dbDisconnect( db , shutdown = TRUE )
 
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Survey%20of%20Income%20and%20Program%20Participation/2008%20panel%20-%20download%20and%20create%20database.R" , prompt = FALSE , echo = TRUE )
 
@@ -39,4 +39,4 @@ warnings()
 db <- dbConnect( MonetDBLite() , dbfolder )
 cs <- dbGetQuery( db , "select tables.name, columns.name, location from tables inner join columns on tables.id=columns.table_id left join storage on tables.name=storage.table and columns.name=storage.column where location is null and tables.name not in ('tables', 'columns', 'users', 'querylog_catalog', 'querylog_calls', 'querylog_history', 'tracelog', 'sessions', 'optimizers', 'environment', 'queue', 'rejects', 'storage', 'storagemodel', 'tablestoragemodel')" )
 print(cs)
-stopifnot(nrow(cs) == 0)
+stopifnot(nrow(cs) == 0) ; dbDisconnect( db , shutdown = TRUE )
