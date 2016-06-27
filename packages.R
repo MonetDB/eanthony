676b6f0c7866392505042cc3852402d4f7209223
Anthony Damico <ajdamico@gmail.com>
2016-06-27 02:05:37 -0400

    Revert "convey on cran"
    This reverts commit 33a83f7f0789cf9bac6e8cd2814806967c1bc435.

--- packages.R
+++ packages.R
@@ -1,4 +1,4 @@
-lp <- c("convey","xml2","rvest","xlsx", "Hmisc", "R.utils", "RCurl", "RSQLite", "SAScii", "XML", "descr", "devtools", "digest", "downloader", "foreign", "gdata", "ggplot2", "httr", "lavaan.survey", "lme4", "mapproj", "maps", "memisc", "mitools", "plyr", "readr", "readxl", "reshape2", "sas7bdat", "sdcMicro", "sqldf", "stringr", "survey", "tools", "ff","ggplot2","scales","fields","mapproj","RColorBrewer","maptools","raster","rgeos","fields","mgcv","spatstat","rgdal","data.table")
+lp <- c("xml2","rvest","xlsx", "Hmisc", "R.utils", "RCurl", "RSQLite", "SAScii", "XML", "descr", "devtools", "digest", "downloader", "foreign", "gdata", "ggplot2", "httr", "lavaan.survey", "lme4", "mapproj", "maps", "memisc", "mitools", "plyr", "readr", "readxl", "reshape2", "sas7bdat", "sdcMicro", "sqldf", "stringr", "survey", "tools", "ff","ggplot2","scales","fields","mapproj","RColorBrewer","maptools","raster","rgeos","fields","mgcv","spatstat","rgdal","data.table")
 repos <- c("http://cran.rstudio.com/")
 # only install if required
 np <- lp[!(lp %in% installed.packages()[,"Package"])]
@@ -6,5 +6,6 @@ if(length(np)) install.packages(np,repos=repos)
 # update if neccessary
 update.packages(repos=repos, ask=F)
 # special snowflakes
+devtools::install_github( "djalmapessoa/convey" )
 devtools::install_github( "hadley/haven" )
 devtools::install_github( "hannesmuehleisen/MonetDBLite" )
