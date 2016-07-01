48f4410bca09d938dee2595f0eb1624c25554785
Anthony Damico <ajdamico@gmail.com>
2016-07-01 02:52:03 -0400

    arf needs dbi

--- arf-test.R
+++ arf-test.R
@@ -1,3 +1,5 @@
+library(DBI)
+
 setwd(Sys.getenv("RWD"))
 options(monetdb.debug.query=T)
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/Area%20Resource%20File/analysis%20examples.R" , prompt = FALSE , echo = TRUE )
