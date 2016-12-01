30794fce07484db3485c850ee09c78ff2eae2394
Anthony Damico <ajdamico@gmail.com>
2016-12-01 06:45:55 -0500

    more years of nvss

--- nvss-setup.R
+++ nvss-setup.R
@@ -7,11 +7,11 @@ if (.Platform$OS.type == 'windows') {
 	path.to.7z <- "7za"															# # only macintosh and *nix users need this line
 }
 
-natality.sets.to.download <- 2014:1999
+natality.sets.to.download <- 2015:1999
 periodlinked.sets.to.download <- 2013:2001
 cohortlinked.sets.to.download <- 2010:1995
 mortality.sets.to.download <- 2014:2000
-fetaldeath.sets.to.download <- 2013:2005
+fetaldeath.sets.to.download <- 2014:2005
 downloader::source_url( "https://raw.githubusercontent.com/ajdamico/asdfree/master/National%20Vital%20Statistics%20System/download%20all%20microdata.R" , prompt = FALSE , echo = TRUE )
 
 warnings()
