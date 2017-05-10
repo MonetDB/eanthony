622fc7351c6c5e316d5a9ebd422813b3e2bcdf91
Anthony Damico <ajdamico@gmail.com>
2017-05-09 22:05:47 -0400

    remove swmap runs for now

--- swmap_pnad-setup.R
+++ swmap_pnad-setup.R
@@ -1,14 +1,2 @@
-setwd( Sys.getenv( "RWD" ) )
 
-machine_specific_replacements <- 
-	list( 
-		
-		# replace the folder path on macnix
-		c( "C:/My Directory" , getwd() ) ,
-		
-		# change other things in the script to be run
-		c( "hello" , "howdy" )
-		
-	)
-
-source( lodown::syntaxtractor( "swmap_pnad" , replacements = machine_specific_replacements , setup_test = "setup" ) , echo = TRUE )
+TRUE
