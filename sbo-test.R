d523422dd467aafac2792ddca01bff6d00d343a5
Anthony Damico <ajdamico@gmail.com>
2017-05-16 11:06:21 -0400

    replaceable quotes

--- sbo-test.R
+++ sbo-test.R
@@ -4,7 +4,7 @@ machine_specific_replacements <-
 	list( 
 		
 		# replace the folder path on macnix
-		c( 'file.path( path.expand( \"~\" ) ) "SBO" )' , getwd() ) ,
+		c( 'file.path( path.expand( \"~\" ) ) \"SBO\" )' , getwd() ) ,
 		
 		# change other things in the script to be run
 		c( "hello" , "howdy" )
