b6795cd08358e18a7582d72f26115e2c104489e9
Anthony Damico <ajdamico@gmail.com>
2017-05-16 14:59:05 -0400

    paste0( '"' , getwd() , '"' )

--- geofabrik-test.R
+++ geofabrik-test.R
@@ -4,7 +4,7 @@ machine_specific_replacements <-
 	list( 
 		
 		# replace the folder path on macnix
-		c( 'file.path( path.expand( \"~\" ) , \"GEOFABRIK\" )' , getwd() ) ,
+		c( 'file.path( path.expand( \"~\" ) , \"GEOFABRIK\" )' , paste0( '"' , getwd() , '"' ) ) ,
 		
 		# change other things in the script to be run
 		c( "hello" , "howdy" )
