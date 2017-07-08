a292cb5ecd87fc0d2163f7f1f0670973072a85b2
Anthony Damico <ajdamico@gmail.com>
2017-07-08 06:17:46 -0400

    convey_devtools

--- convey_devtools-setup.R
+++ convey_devtools-setup.R
@@ -1,14 +1,63 @@
-setwd( Sys.getenv( "RWD" ) )
+setwd(Sys.getenv("RWD"))
 
-machine_specific_replacements <- 
-	list( 
-		
-		# replace the folder path on macnix
-		c( 'path.expand( \"~\" ) , \"CONVEY_DEVTOOLS\"' , paste0( '"' , getwd() , '"' ) ) ,
-		
-		# change other things in the script to be run
-		c( "hello" , "howdy" )
-		
-	)
+library(lodown)
 
-source( lodown::syntaxtractor( "convey_devtools" , replacements = machine_specific_replacements , setup_test = "setup" ) , echo = TRUE )
+tf <- tempfile()
+
+cachaca( "https://github.com/DjalmaPessoa/convey/archive/master.zip" , tf , mode = 'wb' )
+
+for( j in 1:8 ){
+
+	this_tempdir_a <- paste0( tempdir() , j , 'a' )
+	this_tempdir_b <- paste0( tempdir() , j , 'b' )
+	this_tempdir_c <- paste0( tempdir() , j , 'c' )
+	
+	dir.create( this_tempdir_a )
+	dir.create( this_tempdir_b )
+	dir.create( this_tempdir_c )
+	
+	cachaca( "https://github.com/DjalmaPessoa/convey/archive/master.zip" , tf , mode = 'wb' )
+	unzip( tf , exdir = this_tempdir_a )
+	
+	dtt <- devtools::test( paste0( this_tempdir_a , '/convey-master' ) )
+
+	print( dtt )
+
+	if( any( dtt$error ) ) stop( paste0( "devtools::test returned an error on iteration " , j ) )
+
+	unlink( this_tempdir_a , recursive = TRUE ) ; Sys.sleep( 10 )
+	
+	dir.create( this_tempdir_b )
+	cachaca( "https://github.com/DjalmaPessoa/convey/archive/master.zip" , tf , mode = 'wb' )
+	unzip( tf , exdir = this_tempdir_b )
+	
+	dtc <- devtools::check( paste0( this_tempdir_b , '/convey-master' ) , vignettes = FALSE )
+
+	print( dtc )
+
+	if( length( dtc$errors ) > 0 ) stop( paste0( "devtools::check returned an error on iteration " , j ) )
+
+	unlink( this_tempdir_b , recursive = TRUE ) ; Sys.sleep( 10 )
+
+	dir.create( this_tempdir_c )
+	cachaca( "https://github.com/DjalmaPessoa/convey/archive/master.zip" , tf , mode = 'wb' )
+	unzip( tf , exdir = this_tempdir_c )
+	
+	dtcrdt <- devtools::check( paste0( this_tempdir_c , '/convey-master' ) , run_dont_test = TRUE , vignettes = FALSE )
+
+	print( dtcrdt )
+
+	if( length( dtcrdt$errors ) > 0 ) stop( paste0( "devtools::check with run_dont_test=TRUE returned an error on iteration " , j ) )
+
+	unlink( this_tempdir_c , recursive = TRUE ) ; Sys.sleep( 10 )
+	
+}
+
+
+warnings()
+# corruption sniffing
+db <- dbConnect( MonetDBLite::MonetDBLite() )
+cs <- dbGetQuery( db , "select tables.name, columns.name, location from tables inner join columns on tables.id=columns.table_id left join storage on tables.name=storage.table and columns.name=storage.column where location is null and tables.name not in ('tables', 'columns', 'users', 'querylog_catalog', 'querylog_calls', 'querylog_history', 'tracelog', 'sessions', 'optimizers', 'environment', 'queue', 'rejects', 'storage', 'storagemodel', 'tablestoragemodel')" )
+print(cs)
+stopifnot(nrow(cs) == 0) ; dbDisconnect( db , shutdown = TRUE )
+
