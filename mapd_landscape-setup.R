0c6fa184fb0e00f10cbe8f581b45a53cae4b2efc
Anthony Damico <ajdamico@gmail.com>
2017-07-17 13:42:03 -0400

    mapd_landscape

---
+++ mapd_landscape-setup.R
@@ -0,0 +1,14 @@
+setwd( Sys.getenv( "RWD" ) )
+
+machine_specific_replacements <- 
+	list( 
+		
+		# replace the folder path on macnix
+		c( 'path.expand( \"~\" ) , \"MAPD_LANDSCAPE\"' , paste0( '"' , getwd() , '"' ) ) ,
+		
+		# change other things in the script to be run
+		c( "hello" , "howdy" )
+		
+	)
+
+source( lodown::syntaxtractor( "mapd_landscape" , replacements = machine_specific_replacements , setup_test = "setup" ) , echo = TRUE )
