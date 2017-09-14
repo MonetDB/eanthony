998b5cd97c6cb36cfb1eecff953ca90fa6e0c6e5
Hannes Muehleisen <hannes@muehleisen.org>
2017-09-14 15:29:45 -0700

    new packages`

--- packages.R
+++ packages.R
@@ -2,4 +2,13 @@ install.packages(c( "data.table" , "RSQLite" , "devtools" ) , repos="http://cran
 devtools::install_github( "gergness/srvyr" , dependencies = TRUE )
 devtools::install_github( "djalmapessoa/convey" , dependencies = TRUE )
 devtools::install_github( "ajdamico/lodown" , dependencies = TRUE )
-devtools::install_github( "hannesmuehleisen/MonetDBLite" , dependencies = TRUE )
+
+install_submodule_git <- function(x, ...) {
+  install_dir <- tempfile()
+  system(paste("git clone --recursive --depth 1", shQuote(x), 
+shQuote(install_dir)))
+  devtools::install(install_dir, ...)
+}
+install_submodule_git("https://github.com/hannesmuehleisen/MonetDBLite-R")
+
+
