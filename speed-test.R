7590ce23cf2ea47533ff5c6c6eb0466bf7e49d6a
Anthony Damico <ajdamico@gmail.com>
2017-10-02 06:38:44 -0400

    speed fix

--- speed-test.R
+++ speed-test.R
@@ -3,6 +3,6 @@
 # run the exact same convey_testthats again, post-shutdown.
 
 temp_file <- tempfile()
-download.file( "https://raw.githubusercontent.com/ajdamico/eanthony/master/speed-setup.R" , temp_file , mode = 'wb' )
+download.file( "https://raw.githubusercontent.com/MonetDB/eanthony/master/speed-setup.R" , temp_file , mode = 'wb' )
 source( temp_file , echo = TRUE )
 
