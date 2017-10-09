32c1246a4f5639db0246e43f6584b991b91471ef
Hannes Muehleisen <hannes@muehleisen.org>
2017-10-09 16:45:05 +0200

    field changes wrt packages

--- packages.R
+++ packages.R
@@ -1,11 +1,11 @@
 
 install.packages(c( "data.table" , "RSQLite" , "devtools" , "roxygen2" ) , repos="http://cran.rstudio.com/")
 
-update.packages( repos="http://cran.rstudio.com/" , ask = FALSE , quiet = TRUE , type = if ( .Platform$OS.type == 'windows' ) 'win.binary' else 'source' )
+#update.packages( repos="http://cran.rstudio.com/" , ask = FALSE , quiet = TRUE , type = if ( .Platform$OS.type == 'windows' ) 'win.binary' else 'source' )
 
 devtools::install_github( "gergness/srvyr" , dependencies = TRUE )
 devtools::install_github( "djalmapessoa/convey" , dependencies = TRUE )
-devtools::install_github( "ajdamico/lodown" , dependencies = TRUE )
+devtools::install_github( "ajdamico/lodown" , dependencies = TRUE)
 
 install_submodule_git <- function(x, ...) {
   install_dir <- tempfile()
