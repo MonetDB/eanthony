53a5b96926853cec69ad4c9a35340d65f9d34d2b
Hannes Mühleisen <hannes@muehleisen.org>
2017-11-06 06:59:57 +0100

    Update convey_devtools-test.R
    No point running this again

--- convey_devtools-test.R
+++ convey_devtools-test.R
@@ -1,8 +1,2 @@
 
-
-# run the exact same convey_testthats again, post-shutdown.
-
-temp_file <- tempfile()
-download.file( "https://raw.githubusercontent.com/MonetDB/eanthony/master/convey_devtools-setup.R" , temp_file , mode = 'wb' )
-source( temp_file , echo = TRUE )
-
+# NOP
