e8a8ca3e5327aa0f6119608a60cbc6f5e71cd92a
Anthony Damico <ajdamico@gmail.com>
2017-11-16 08:23:25 -0500

    maintain sisyphus

--- packages.R
+++ packages.R
@@ -4,9 +4,11 @@ install_or_upgrade_packages <- function(lp, ...) {
 	update.packages(ask=F, oldPkgs=lp, ...)
 }
 
-install_or_upgrade_packages(c("data.table", "RSQLite" , "devtools" , "roxygen2", "srvyr", "convey", "DBI", "digest", "testthat", "vardpoor", "IC2", "dbplyr","RODBC"), repos='http://cran.rstudio.com/', quiet=F,dependencies=TRUE)
+install_or_upgrade_packages(c("data.table", "RSQLite" , "devtools" , "roxygen2", "srvyr", "DBI", "digest", "testthat", "vardpoor", "IC2", "dbplyr","RODBC"), repos='http://cran.rstudio.com/', quiet=F,dependencies=TRUE)
 
-devtools::install_github("ajdamico/lodown" , dependencies = TRUE)
+devtools::install_github("hannesmuehleisen/convey" , dependencies = TRUE)
+
+devtools::install_github("hannesmuehleisen/lodown" , dependencies = TRUE)
 
 install_submodule_git <- function(x, ...) {
   install_dir <- tempfile()
