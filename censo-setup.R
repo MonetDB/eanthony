270753cdb4f7eddd15234e3f9663bee85c23b6a4
Hannes Muehleisen <hannes@muehleisen.org>
2018-06-19 09:59:18 +0200

    censo only 2010

--- censo-setup.R
+++ censo-setup.R
@@ -1,14 +1,4 @@
 setwd( Sys.getenv( "RWD" ) )
-
-machine_specific_replacements <- 
-	list( 
-		
-		# replace the folder path on macnix
-		c( 'path.expand( \"~\" ) , \"CENSO\"' , paste0( '"' , getwd() , '"' ) ) ,
-		
-		# change other things in the script to be run
-		c( "hello" , "howdy" )
-		
-	)
-
-source( lodown::syntaxtractor( "censo" , replacements = machine_specific_replacements , setup_test = "setup" ) , echo = TRUE )
+a <- lodown::get_catalog("censo")
+# don't do 2000 it's been broken forever
+lodown::lodown("censo", catalog=a[a$year == 2010,], output_dir= getwd())
