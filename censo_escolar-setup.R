f624b5b40783dfb0f2c9b4e86e86dd319c26226c
Hannes Muehleisen <hannes@muehleisen.org>
2018-06-25 09:17:02 +0200

    mc

--- censo_escolar-setup.R
+++ censo_escolar-setup.R
@@ -2,4 +2,4 @@ setwd( Sys.getenv( "RWD" ) )
 
 a <- lodown::get_catalog("censo_escolar")
 # don't do 2017 it's been broken forever
-lodown::lodown("censo", catalog=a[a$year < 2017,], output_dir= getwd())
+lodown::lodown("censo_escolar", catalog=a[a$year < 2017,], output_dir= getwd())
